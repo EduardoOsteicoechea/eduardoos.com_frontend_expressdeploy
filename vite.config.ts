5ae42ddaacfde7bfa2d7bacd4087ab02e6b94466
eduar <eduar@EDUARDO>
2026-03-26 11:35:24 -0400

    cleaning by .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+   plugins: [sveltekit()],
    server: {
-		// Required for Docker so it exposes the port outside the container
-		host: true, 
-		proxy: {
-			// Any request starting with /api will be forwarded to your Node backend
-			'/api': {
-				target: 'http://backend:3000', 
-				changeOrigin: true,
-				// Optional: If your backend routes don't start with /api, you can rewrite the path
-				// rewrite: (path) => path.replace(/^\/api/, '') 
-			}
-		}
-	}
+      host: true, 
+      // Add your Railway URL here to allow traffic
+      allowedHosts: ['eduardooscomfrontendexpressdeploy-production.up.railway.app'],
+      proxy: {
+         '/api': {
+            target: 'http://backend:3000', 
+            changeOrigin: true,
+         }
+      }
+   }
 });
